1ce75d16289c9d72bae3dbdf739968878e99abd9
Victor Madera <72475885+Klaz1k@users.noreply.github.com>
2024-12-02 22:36:09 -0400

    fix(user): Changed wording for requested user credentials not matching error

--- src/user/application/errors/user_credentials_not_matching.py
+++ src/user/application/errors/user_credentials_not_matching.py
@@ -3,5 +3,5 @@ from user.application.errors.codes.user_error_codes import UserErrorCodes
 
 
 user_credentials_not_matching_error = application_error_factory(
-  code= UserErrorCodes.INCORRECT_CREDENTIALS.value, message= "Credentials for updating user did not match"
+  code= UserErrorCodes.INCORRECT_CREDENTIALS.value, message= "Credentials for user in request did not match"
 )
