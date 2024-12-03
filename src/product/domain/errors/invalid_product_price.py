eaa073bdac90dee50e3c9f1ddad6630dcfc2806c
AndyMine12 <andresale.fq@gmail.com>
2024-12-03 19:31:43 -0400

    fix(product): :bug: Fix incorrect error names
    Fixed incorrect error naming for invalid product id and price

--- src/product/domain/errors/invalid_product_price.py
+++ src/product/domain/errors/invalid_product_price.py
@@ -1,7 +1,7 @@
 from common.domain.error.domain_error import domain_error_factory
 
-INVALID_PRODUCT_PRICE_ERROR = "invalid_price_name"
+INVALID_PRODUCT_PRICE_ERROR = "invalid_product_price"
 
 invalid_product_price_error = domain_error_factory(
-    INVALID_PRODUCT_PRICE_ERROR, "Prices starts at 0.00"
+    INVALID_PRODUCT_PRICE_ERROR, "Prices start at 0.00"
 )
