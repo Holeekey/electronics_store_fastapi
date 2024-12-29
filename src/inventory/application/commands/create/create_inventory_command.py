1bc9099cc947900c840e4c6d06384883c954d243
Andrés Arispe <andresarispe2605@gmail.com>
2024-12-29 15:35:39 -0400

    feat(inventory): :art: Command to create or update an invetory

--- src/inventory/application/commands/create/create_inventory_command.py
+++ src/inventory/application/commands/create/create_inventory_command.py
@@ -1,28 +1,34 @@
 from common.application.id_generator.id_generator import IDGenerator
 from common.domain.result.result import Result
 from common.application.service.application_service import IApplicationService
-from inventory.application.commands.create.types.dto import CreateInventoryDto
-from inventory.application.commands.create.types.response import CreateInventoryResponse
+from inventory.application.commands.types.createInventorydto import CreateInventoryDto
+from inventory.application.commands.types.response import CreateInventoryResponse
+from inventory.application.info import inventory_updated_info
 from inventory.application.repositories.inventory_repository import IInventoryRepository
 from inventory.application.info.inventory_created_info import inventory_created_info
 from inventory.application.models.inventory import Inventory
-from inventory.application.errors.inventory_already_exists import (
-    inventory_already_exists_error,
-)
 
 
-class CreateUserCommand(IApplicationService):
+class CreateOrUpdateInventoryCommand(IApplicationService):
 
     def __init__(self, id_generator: IDGenerator, inventory_repository: IInventoryRepository):
-        self._user_repository = inventory_repository
+        self._inventory_repository = inventory_repository
         self._id_generator = id_generator
 
     async def execute(self, data: CreateInventoryDto) -> Result[CreateInventoryResponse]:
 
-        if await self._inventory_repository.find_by_product_id(product_id=data.product_id):
-            return Result.failure(error=inventory_already_exists_error())
+        existing_inventory = await self._inventory_repository.find_by_product_id(product_id=data.product_id)
 
+        if existing_inventory:
+            # Actualizar el stock completo si el inventario ya existe
+            existing_inventory.stock = data.stock
+            await self._inventory_repository.update(existing_inventory)
+            return Result.success(
+                value=CreateInventoryResponse(id=existing_inventory.id),
+                info=inventory_updated_info()
+            )
 
+        # Crear un nuevo inventario si no existe
         inventory = Inventory(
             id=self._id_generator.generate(),
             product_id=data.product_id,
