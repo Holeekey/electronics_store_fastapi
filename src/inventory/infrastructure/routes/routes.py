4903c9d39e2a1fdaf8434d0731249e2234cc1c60
Andrés Arispe <andresarispe2605@gmail.com>
2024-12-29 15:59:11 -0400

    feat(inventory): :sparkles: endpoints implemented

--- src/inventory/infrastructure/routes/routes.py
+++ src/inventory/infrastructure/routes/routes.py
@@ -2,15 +2,19 @@ from uuid import UUID
 from fastapi import APIRouter
 
 from common.application.decorators.error_decorator import ErrorDecorator
+from common.application.decorators.logger_decorator import LoggerDecorator
 from common.infrastructure.id_generator.uuid.uuid_generator import UUIDGenerator
+from common.infrastructure.loggers.loguru_logger import LoguruLogger
 from common.infrastructure.responses.handlers.error_response_handler import (
     error_response_handler,
 )
 from common.infrastructure.responses.handlers.success_response_handler import (
     success_response_handler,
 )
+from inventory.application.commands.adjust.adjust_inventory_command import AdjustInventoryCommand
 from inventory.application.commands.create.create_inventory_command import (
     CreateInventoryCommand,
+    CreateOrUpdateInventoryCommand,
 )
 from inventory.application.queries.find_one_product_query import (
     FindOneProductQuery,
@@ -20,6 +24,7 @@ from inventory.infrastructure.repositories.postgres.sqlalchemy.inventory_reposit
     InventoryRepositorySqlAlchemy,
 )
 from inventory.infrastructure.routes.types.dto.create.create_inventory_dto import CreateInventoryDto
+from inventory.infrastructure.routes.types.dto.create.adjust_inventory_dto import AdjustInventoryDto
 
 
 inventory_router = APIRouter(
@@ -35,7 +40,10 @@ inventory_repository = InventoryRepositorySqlAlchemy()
 async def find_inventory_by_product(id: UUID):
 
     result = await ErrorDecorator(
-        service=FindOneProductQuery(inventory_repository=inventory_repository),
+        service= LoggerDecorator(
+            service=FindOneProductQuery(inventory_repository=inventory_repository),
+            loggers=[LoguruLogger("Find Inventory")]
+        ),
         error_handler=error_response_handler,
     ).execute(data=FindOneInventoryDto(id=id.__str__()))
 
@@ -43,31 +51,38 @@ async def find_inventory_by_product(id: UUID):
 
 
 @inventory_router.post("/{product_id}")
-async def update_inventory(body: CreateInventoryDto):
-
-    # idGenerator = RandomIdGenerator()
+async def create_or_update_inventory(product_id: str, body: CreateInventoryDto):
+    
     idGenerator = UUIDGenerator()
-
     result = await ErrorDecorator(
-        service=CreateInventoryCommand(
-            id_generator=idGenerator, product_repository=inventory_repository
-        ),
+        LoggerDecorator(
+            service=CreateOrUpdateInventoryCommand(
+                id_generator=idGenerator,
+                invetory_repository=inventory_repository
+            ),
+            loggers=[LoguruLogger("Update inventory")]
+        ), 
         error_handler=error_response_handler,
-    ).execute(data=body)
+    ).execute(product_id=product_id, data=body)
 
     return result.handle_success(handler=success_response_handler)
 
-@inventory_router.put("/{product_id}")
-async def update_inventory(body: CreateInventoryDto):
 
-    # idGenerator = RandomIdGenerator()
+@inventory_router.put("/{product_id}")
+async def adjust_inventory(product_id: str, body: AdjustInventoryDto):
+   
     idGenerator = UUIDGenerator()
 
+    
     result = await ErrorDecorator(
-        service=CreateInventoryCommand(
-            id_generator=idGenerator, product_repository=inventory_repository
+        service= LoggerDecorator(
+            service= AdjustInventoryCommand(
+                id_generator=idGenerator,
+                product_repository=inventory_repository
+            ),
+            loggers=[LoguruLogger("Adjust inventory")]
         ),
         error_handler=error_response_handler,
-    ).execute(data=body)
+    ).execute(product_id=product_id, data=body)
 
     return result.handle_success(handler=success_response_handler)
