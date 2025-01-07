97cfcbd0410b225764286ef2ccc2345aa6a92550
Holeekey <danbrunosv@gmail.com>
2025-01-07 13:51:23 -0400

    fix(product): :bug: fix update product

--- src/product/infrastructure/routes/types/create_product_dto.py
+++ src/product/infrastructure/routes/types/create_product_dto.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class CreateProductDto(BaseModel):
-    code: str
-    name: str
+    code: str = Field(min_length=1)
+    name: str = Field(min_length=3)
     description: str
-    cost: float
-    margin: float
+    cost: float = Field(gt=0)
+    margin: float = Field(gt=0,lt=1)
--- src/product/infrastructure/routes/types/update_product_dto.py
+++ src/product/infrastructure/routes/types/update_product_dto.py
@@ -1,9 +1,10 @@
-from pydantic import BaseModel
+from typing import Optional
+from pydantic import BaseModel, Field
 
 
 class UpdateProductQueryDto(BaseModel):
-    code: str
-    name: str
-    description: str
-    cost: float
-    margin: float
+    code: Optional[str] = Field(min_length=1, default=None)
+    name: Optional[str] = Field(min_length=3, default=None)
+    description: Optional[str] = Field(default=None)
+    cost: Optional[float] = Field(gt=0, default=None)
+    margin: Optional[float] = Field(gt=0,lt=1, default=None)
