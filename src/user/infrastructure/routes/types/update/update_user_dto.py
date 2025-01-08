b0c61269d0b0f175f9ac4057f5d9ca66d77f412f
Holeekey <danbrunosv@gmail.com>
2025-01-07 22:13:53 -0400

    fix(user): :bug: update user dto email validation

--- src/user/infrastructure/routes/types/update/update_user_dto.py
+++ src/user/infrastructure/routes/types/update/update_user_dto.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, EmailStr, Field
 
 from src.user.infrastructure.routes.types.create.create_user_dto import UserRole
 
@@ -12,7 +12,7 @@ class UserStatus(str, Enum):
 class UpdateUserDto(BaseModel):
   id: str
   username: Optional[str] = Field(min_length=3, default=None)
-  email: Optional[str] = Field(default=None)
+  email: Optional[EmailStr] = Field(default=None)
   password: Optional[str] = Field(min_length=6, default=None)
   first_name: Optional[str] = Field(min_length=1, default=None)
   last_name: Optional[str] = Field(min_length=1, default=None)
